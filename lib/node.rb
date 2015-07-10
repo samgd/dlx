b5862c18f812be8e7788077603217f8308a4e195
Sam Davis <sam@samgd.com>
2015-07-10 20:53:06 +0100

    Implemented removing and restoring of nodes from matrix

--- lib/node.rb
+++ lib/node.rb
@@ -1,3 +1,28 @@
 class Node
   attr_accessor :up, :right, :down, :left
+
+  def initialize(up = self, right = self, down = self, left = self)
+  end
+
+  def remove(type)
+    case type
+    when :row
+      left.right = right
+      right.left = left
+    when :column
+      up.down = down
+      down.up = up
+    end
+  end
+
+  def restore(type)
+    case type
+    when :row
+      left.right = self
+      right.left = self
+    when :column
+      up.down = self
+      down.up = self
+    end
+  end
 end
