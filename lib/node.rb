b4abf2e07cf4e0883a4b6dd15f2322a1ccedb271
Sam Davis <sam@samgd.com>
2015-07-10 21:32:54 +0100

    Initialize surrounding nodes to self

--- lib/node.rb
+++ lib/node.rb
@@ -2,6 +2,10 @@ class Node
   attr_accessor :up, :right, :down, :left
 
   def initialize(up = self, right = self, down = self, left = self)
+    @up = up
+    @right = right
+    @down = down
+    @left = left
   end
 
   def link(nodes)
