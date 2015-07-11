ceebc5f6eef0ad8d5143472427b38fb6571947ea
Sam Davis <sam@samgd.com>
2015-07-11 16:50:56 +0100

    Header node total added

--- lib/header.rb
+++ lib/header.rb
@@ -1,4 +1,19 @@
 require_relative 'node'
 
 class Header < Node
+
+  def total
+    @total ||= calc_total
+  end
+
+  private
+
+  def calc_total
+    node = self
+    @total = 0
+    until (node = node.down) == self do
+      @total += 1
+    end
+    @total
+  end
 end
